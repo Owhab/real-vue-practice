781d33ab76dffa96b7a7b30fe339b8144055e7d2
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-11-01 11:33:41 +0600

    working with single file component in vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <EventCard/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EventCard from './components/EventCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EventCard
   }
 }
 </script>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="event-card">
+    <h1>Hello World</h1>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+ /*  props: {
+    msg: String
+  } */
+
+  data(){
+    return{
+      event: {
+        id: 5928101,
+        category: "animal welfare",
+        title: "Cat Adoptation Day",
+        description: "FInd your new feline friend at this event",
+        location: "Meow Town",
+        data: "January 28, 2022",
+        time: "12:00",
+        petsAllowed: true,
+        organizer: "Kal Laydee"
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.event-card{
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #;
+}
+
+</style>
